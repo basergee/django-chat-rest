76119f6574a5d7d06fa3d119fd0533ae2dcbf761
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-09-28 02:37:39 +0500

    Добавил вывод списка комнат

--- chatrest/chat/views.py
+++ chatrest/chat/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.views.generic import CreateView, TemplateView
 
 from .forms import UserSignupForm
+from .models import Room
 
 
 class UserSignupView(CreateView):
@@ -15,6 +16,11 @@ class UserSignupView(CreateView):
 
 class IndexView(TemplateView):
     template_name = "chat/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rooms'] = Room.objects.all()
+        return context
 
 
 def room(request, room_name):
